0edcaec1aae86ff937e2b70a6a209f6e0a7537dd
guhdalla <guhdalla011@gmail.com>
2020-05-15 17:14:04 -0300

    Numero 1 concluido

---
+++ ProgramaCPF.py
@@ -0,0 +1,35 @@
+num = 1
+while num == 1 or num == 2 or num==3:
+    print("|___Menu de Opções___|")
+    print("1. Gerar digitos verificadores")
+    print("2. Verificar digitos verificadores")
+    print("3. Estado de Origem")
+    print("4. Sair")
+    num = int(input("Digite a opção desejada: "))
+    while num > 4 or num < 1:
+        num = int(input("Opção desejada invalida, digite novamente: "))
+    if num == 1:
+        CPF = int(input("Digite o numero do CPF sem os 2 ultimos digitos: "))
+        while CPF > 999999999 or CPF < 100000000:
+            CPF = int(input("Numero do CPF digitado invalido, digite novamente: "))
+        # Calcular digito verificador J
+        CPF = str(CPF)
+        somaJ = int(CPF[0])*10 + int(CPF[1])*9 + int(CPF[2])*8 + int(CPF[3])*7 + int(CPF[4])*6 + int(CPF[5])*5 + int(CPF[6])*4 + int(CPF[7])*3 + int(CPF[8])*2
+        divJ = somaJ % 11
+        digJ = 0
+        if divJ < 2:
+            digJ = 0
+        else:
+            digJ = 11 - divJ
+        CPF = int(CPF) * 10 + digJ
+        #Calcular digito verificador K
+        CPF = str(CPF)
+        somaK = int(CPF[0])*11 + int(CPF[1])*10 + int(CPF[2])*9 + int(CPF[3])*8 + int(CPF[4])*7 + int(CPF[5])*6 + int(CPF[6])*5 + int(CPF[7])*4 + int(CPF[8])*3 + int(CPF[9])*2
+        divK = somaK % 11
+        digK = 0
+        if divK < 2:
+            digK = 0
+        else:
+            digK = 11 - divK
+        CPF = int(CPF) * 10 + digK
+        print("O numero do CPF: ", CPF)
---
+++ Teste.py
@@ -0,0 +1,13 @@
+#for i in range(2, 12):
+#    somaK = int(CPF[0]) * i[-1] + int(CPF[1]) * i[-2] + int(CPF[2]) * i[-3] + int(CPF[3]) * i[-4] + int(CPF[4]) * i[
+#        -5] + int(CPF[5]) * i[-6] + int(CPF[6]) * i[-7] + int(CPF[7]) * i[-8] + int(CPF[8]) * i[-9]
+#divK = somaK % 11
+#digK = 0
+#if divK < 2:
+#    digK = 0
+#    print(digK)
+#else:
+#    digK = 11 - divK
+#    print(digK)
+CPF = 423215688 * 10 + 7
+print(CPF)
